1ccd73c53eff515dc7c0145b7f19d3b8a19d0dae
Leonel Angel <leonelgo66@gmail.com>
2024-09-01 01:50:01 -0300

    feat:  added models

---
+++ server/src/models/pets.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const petSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  specie: { type: String, enum: ['perro', 'gato'], required: true },
+  breed: { type: String, required: true },
+  description: { type: String, required: true },
+  size: { type: String, required: true },
+  healthStatus: {
+    type: String,
+    enum: ['healthy', 'sick', 'treatment', 'recovered'],
+    default: 'healthy'
+  },
+  idDeleted: { type: Boolean, default: false },
+  imgs: [String],
+  category: { type: String, enum: ['perros', 'gatos'], required: true },
+  userAdmin: { type: Schema.Types.ObjectId, ref: 'Users', required: true },
+  post: { type: Boolean, required: true }
+});
+
+const petModel = mongoose.model('Pets', petSchema);
+
+export default petModel;
