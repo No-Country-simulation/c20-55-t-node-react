8fcd2c4da27adffdafaab8c78c80d8da01a3c51b
NicolasKowal <nicolas.kowal95@gmail.com>
2024-09-03 16:39:37 -0300

    descrpition pasando parametro

--- client/src/app/users/adopter/home/description/page.jsx
+++ client/src/app/users/adopter/home/description/page.jsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { useSearchParams } from "next/navigation";
 import Link from "next/link";
 import React from "react";
 
 function ItemDescription() {
+	const searchParams = useSearchParams();
+	const descripcion = searchParams.get("descripcion");
+
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center bg-gray-100 p-4">
-			<h2>detalle</h2>
-			<Link href={"/users/adopter/home"}>volver</Link>
+			<h2>Detalle</h2>
+			<p>{descripcion || "No hay descripción disponible"}</p>
+			<Link href="/users/adopter/home">Volver</Link>
 		</div>
 	);
 }
