19e00153cfb67df0a41e27627fb0aa494cff37c1
Leonel Angel <leonelgo66@gmail.com>
2024-09-06 21:18:53 -0300

    feat: add token to session

---
+++ server/src/middlewares/tokenToSession.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import { SECRET_JWT_KEY } from "../config/config.js";
+
+export const tokenToSession = (req, res, next) => {
+    const token = req.cookies.accessToken;
+
+    req.session = { userData: null };
+
+    try {
+        const userData = jwt.verify(token, SECRET_JWT_KEY);
+        req.session.userData = userData;
+    } catch (error) {
+        console.error("Error verifying token:", error);
+    }
+    next();
+};
