77b3ff72efb4f27d38de39fdf0bada0578de798e
Leonel Angel <leonelgo66@gmail.com>
2024-09-06 21:17:50 -0300

    feat: register adopter and admin, added logout, added to swagger

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,15 +1,37 @@
-import userModel from '../models/users.js';
+import userModel from "../models/users.js";
 
 export default class AuthService {
-  async getUserByEmail(email) {
-    try {
-      const user = await userModel.findOne({ email });
-      if (!user) {
-        throw new Error(`No user found with email: ${email}`);
-      }
-      return user;
-    } catch (error) {
-      throw new Error(error.message);
+    async createUser({ email, password, role }) {
+        try {
+            const existUser = await this.checkIfUserExists(email);
+            
+            if (existUser) {
+                throw new Error("User with this email already exists");
+            }
+
+            const user = new userModel({ email, password, role });
+            user.save();
+            return user;
+        } catch (error) {
+            throw new Error(`Error creating user: ${error.message}`);
+        }
     }
-  }
+
+    async getUserByEmail(email) {
+        return this.findUserByEmail(email);
+    }
+
+    async findUserByEmail(email) {
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+            throw new Error(`No user found with email: ${email}`);
+        }
+        return user;
+    }
+
+    async checkIfUserExists(email) {
+        const user = await userModel.findOne({ email });
+        return user;
+    }
 }
