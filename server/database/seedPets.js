23947deb3dbd2c7c9e7e3fbc467070f719d121a0
Leonel Angel <leonelgo66@gmail.com>
2024-09-08 20:43:00 -0300

    feat: install multer library, inplement seeds

---
+++ server/database/seedPets.js
@@ -0,0 +1,229 @@
+export const seedPets = [
+    {
+        name: "Max",
+        age: 5,
+        specie: "perro",
+        breed: "Labrador Retriever",
+        description:
+            "Max es un Labrador amistoso y enérgico, perfecto para familias activas.",
+        size: "Grande",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://labradoresdeabantueso.com/wp-content/uploads/2023/09/Foto-Labrador-1-Guia-min.jpg",
+            "https://labradoresdeabantueso.com/wp-content/uploads/2023/09/Foto-Labrador-1-Guia-min.jpg",
+            "https://labradoresdeabantueso.com/wp-content/uploads/2023/09/Foto-Labrador-1-Guia-min.jpg"
+        ],
+        category: "perros",
+        post: true
+    },
+    {
+        name: "Luna",
+        age: 3,
+        specie: "gato",
+        breed: "Siamés",
+        description:
+            "Luna es una gata elegante y tranquila, ideal para un hogar sereno.",
+        size: "Mediano",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://t2.uc.ltmcdn.com/es/posts/3/2/5/como_cuidar_a_un_gato_siames_34523_orig.jpg",
+            "https://t2.uc.ltmcdn.com/es/posts/3/2/5/como_cuidar_a_un_gato_siames_34523_orig.jpg"
+        ],
+        category: "gatos",
+        post: true
+    },
+    {
+        name: "Rocky",
+        age: 4,
+        specie: "perro",
+        breed: "Bulldog",
+        description:
+            "Rocky es un Bulldog cariñoso, conocido por su lealtad y carácter tranquilo.",
+        size: "Mediano",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://images.pagina12.com.ar/styles/focal_3_2_960x640/public/2022-06/560550-bulldog_0.jpg?h=b10b0591&itok=_EjQxNg8"
+        ],
+        category: "perros",
+        post: true
+    },
+    {
+        name: "Mia",
+        age: 2,
+        specie: "gato",
+        breed: "Persa",
+        description: "Mia es una gata Persa de pelo largo, dulce y juguetona.",
+        size: "Pequeño",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://www.anicura.es/cdn-cgi/image/f=auto,q=60,fit=cover,w=1536,h=1152,g=auto,sharpen=1/AdaptiveImages/optimizely/f20f5564-9222-4428-b918-c4242fa4fc45/print_anicura_cats_210_persian.jpg?stamp=3385d9f235626b7e51d5fd82ce5fe538fe579a7f"
+        ],
+        category: "gatos",
+        post: true
+    },
+    {
+        name: "Charlie",
+        age: 6,
+        specie: "perro",
+        breed: "Golden Retriever",
+        description:
+            "Charlie es un Golden Retriever amable y juguetón, perfecto para familias.",
+        size: "Grande",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://upload.wikimedia.org/wikipedia/commons/9/93/Golden_Retriever_Carlos_%2810581910556%29.jpg",
+            "https://upload.wikimedia.org/wikipedia/commons/9/93/Golden_Retriever_Carlos_%2810581910556%29.jpg"
+        ],
+        category: "perros",
+        post: true
+    },
+    {
+        name: "Simba",
+        age: 4,
+        specie: "gato",
+        breed: "Bengalí",
+        description:
+            "Simba es un gato Bengalí curioso y enérgico, con un patrón de pelaje único.",
+        size: "Mediano",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://0d2ujxjiqkxw.cdn.shift8web.com/wp-content/uploads/Gato-bengal%C3%AD.jpg"
+        ],
+        category: "gatos",
+        post: true
+    },
+    {
+        name: "Bella",
+        age: 3,
+        specie: "perro",
+        breed: "Beagle",
+        description:
+            "Bella es una Beagle amigable y curiosa, siempre lista para una aventura.",
+        size: "Mediano",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://i0.wp.com/kennelclubargentino.org.ar/storage/2019/08/cachorro-beagle.jpg?fit=769%2C630&ssl=1"
+        ],
+        category: "perros",
+        post: true
+    },
+    {
+        name: "Oliver",
+        age: 2,
+        specie: "gato",
+        breed: "Maine Coon",
+        description:
+            "Oliver es un Maine Coon grande y majestuoso, conocido por su sociabilidad.",
+        size: "Grande",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://www.zooplus.es/magazine/wp-content/uploads/2018/08/maine-coon-3.jpg"
+        ],
+        category: "gatos",
+        post: true
+    },
+    {
+        name: "Duke",
+        age: 7,
+        specie: "perro",
+        breed: "Rottweiler",
+        description:
+            "Duke es un Rottweiler fuerte y protector, ideal para la seguridad del hogar.",
+        size: "Grande",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR5-gDEpl5Q9cfWe7l-DdIIrOBApfA7b74J-aiSYYGgInaoQKl_"
+        ],
+        category: "perros",
+        post: true
+    },
+    {
+        name: "Lily",
+        age: 3,
+        specie: "gato",
+        breed: "Scottish Fold",
+        description:
+            "Lily es una gata Scottish Fold tranquila y cariñosa, perfecta para hogares pequeños.",
+        size: "Pequeño",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTV_434_yNRDenvmchxKziVuiKIi1EjDueQ-Lgsee4IGwpbukr2"
+        ],
+        category: "gatos",
+        post: true
+    },
+    {
+        name: "Toby",
+        age: 5,
+        specie: "perro",
+        breed: "Dachshund",
+        description:
+            "Toby es un Dachshund curioso y valiente, ideal para personas activas.",
+        size: "Pequeño",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQUIubYs-kyZCnEF1b6yK1FFknSYz0L8vAj6C5pv2ol1QaSo5NM"
+        ],
+        category: "perros",
+        post: true
+    },
+    {
+        name: "Oscar",
+        age: 4,
+        specie: "gato",
+        breed: "Esfinge",
+        description:
+            "Oscar es un gato Esfinge afectuoso y único, con una apariencia sin pelo.",
+        size: "Mediano",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://www.purina.com.ar/sites/default/files/2022-11/purina-como-cuidar-al-gato-esfinge-y-otras-razas-de-gatos-sin-pelo-nota_04.jpg"
+        ],
+        category: "gatos",
+        post: true
+    },
+    {
+        name: "Bruno",
+        age: 6,
+        specie: "perro",
+        breed: "Doberman",
+        description:
+            "Bruno es un Doberman protector y valiente, ideal para seguridad y compañía.",
+        size: "Grande",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQpDAQATW2gWbMDKV5U9k0ZJQcRJl7bUPodVynrvCjgVovJXvZu"
+        ],
+        category: "perros",
+        post: true
+    },
+    {
+        name: "Nala",
+        age: 3,
+        specie: "gato",
+        breed: "Himalayo",
+        description:
+            "Nala es una gata Himalayo serena y dulce, perfecta para una vida tranquila.",
+        size: "Mediano",
+        healthStatus: "sano",
+        idDeleted: false,
+        imgs: [
+            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTO2QMaGsrIYkaur9M9H1lT0-xMoPtqcLicIkUT_AbesALMNaXA"
+        ],
+        category: "gatos",
+        post: true
+    }
+];
