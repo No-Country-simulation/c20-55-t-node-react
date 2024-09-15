1c45a5137593c42b4f78f75fccc9c60ae89d1de9
Leonel Angel <leonelgo66@gmail.com>
2024-09-14 23:34:23 -0300

    feat: add seed for application and users

--- server/database/seed.js
+++ server/database/seed.js
@@ -2,12 +2,48 @@ import mongoose from "mongoose";
 import petModel from "../src/models/pets.js";
 import { seedPets } from "./seedPets.js";
 import connectToMongoDB from "../src/config/database.js";
+import userModel from "../src/models/users.js";
+import { seedUsers } from "./seedUsers.js";
+import applicationModel from "../src/models/applications.js";
+import { seedApplications } from "./seedApplication.js";
 
 async function seedDatabase() {
     connectToMongoDB();
 
     await petModel.deleteMany({});
     await petModel.insertMany(seedPets);
+    await userModel.deleteMany({});
+    await userModel.insertMany(seedUsers);
+
+    const pets = await petModel.find({});
+
+    const updatedApplications = await Promise.all(
+        seedApplications.map(async (application) => {
+            const adopterName = application.adopterInfo.name
+                .split(" ")[0]
+                .toLocaleLowerCase()
+                .normalize("NFD")
+                .replace(/[\u0300-\u036f]/g, "")
+                .toLowerCase();
+
+            const user = await userModel.findOne({
+                email: {
+                    $regex: new RegExp(adopterName, "i")
+                }
+            });
+
+            const petIndex = parseInt(Math.random() * pets.length);
+
+            return {
+                ...application,
+                adopterId: user._id,
+                petId: pets[petIndex]._id
+            };
+        })
+    );
+
+    await applicationModel.deleteMany({});
+    await applicationModel.insertMany(updatedApplications);
     console.log("Datos de prueba insertados");
     mongoose.connection.close();
 }
