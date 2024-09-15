e4d12bccb74732324487bb7f8167dbfa8324fa16
Leonel Angel <leonelgo66@gmail.com>
2024-09-14 23:37:03 -0300

    refactor: comment some atribute and change defalut value of status the pet

--- server/src/models/applications.js
+++ server/src/models/applications.js
@@ -1,32 +1,44 @@
 import mongoose from 'mongoose';
 
+const { Schema } = mongoose;
+
 const applicationSchema = new mongoose.Schema({
   adopterId: { type: Schema.Types.ObjectId, ref: 'Users', required: true },
   petId: { type: Schema.Types.ObjectId, ref: 'Pets', required: true },
   status: {
     type: String,
-    enum: ['pending', 'approved', 'rejected', 'moreInfo', 'adopted'],
-    default: 'pending'
+    // enum: ['pending', 'approved', 'rejected', 'moreInfo', 'adopted'],
+    default: 'Pendiente'
   },
   applicationDate: { type: Date, default: Date.now },
   adopterInfo: {
     name: { type: String, required: true },
     age: { type: Number },
     phone: { type: String },
-    email: { type: String, required: true },
-    housingType: { type: String, enum: ['house', 'apartment'] },
-    housingOwnership: { type: String, enum: ['owned', 'rented'] },
+    // email: { type: String, required: true },
+    nationalID: { type: String },
+    housingType: {
+      type: String,
+      // enum: ['house', 'apartment']
+    },
+    housingOwnership: {
+      type: String,
+      // enum: ['owned', 'rented']
+    },
     outdoorSpace: { type: Boolean },
-    numPeople: { type: Number },
+    totalFamily: { type: Number },
     hasChildren: { type: Boolean },
     otherPets: { type: Boolean },
+    typePets: { type: String },
     hadPets: { type: Boolean },
+    sizeHasPets: { type: String },
     surrenderPet: { type: Boolean },
-    reasonSurrender: { type: Boolean },
+    reasonSurrender: { type: String },
     commitments: {
       coverCosts: { type: Boolean, required: true },
       returnPet: { type: Boolean, required: true }
-    }
+    },
+    contract: { type: String },
   }
 });
 
