61a2a03c3206ea257cb063dee9c684a5217ecb3e
Leonel Angel <leonelgo66@gmail.com>
2024-09-18 10:15:55 -0300

    feat: add more data for seedUsers

--- server/database/seedUsers.js
+++ server/database/seedUsers.js
@@ -4,26 +4,41 @@ export const seedUsers = [
     {
         email: "leonel@example.com",
         password: createHash("12345Ll$"),
-        role: "adopter"
+        role: "adopter",
+        name: "Leonel",
+        surname: "Vargas",
+        age: 30
     },
     {
         email: "camila@example.com",
         password: createHash("12345Ll$"),
-        role: "adopter"
+        role: "adopter",
+        name: "Camila",
+        surname: "Magallanes",
+        age: 22
     },
     {
         email: "andres@example.com",
         password: createHash("12345Ll$"),
-        role: "adopter"
+        role: "adopter",
+        name: "Andres",
+        surname: "Campos",
+        age: 25
     },
     {
         email: "angel@example.com",
         password: createHash("12345Ll$"),
-        role: "adopter"
+        role: "adopter",
+        name: "Angel",
+        surname: "Zumbay",
+        age: 35
     },
     {
         email: "agustin@example.com",
         password: createHash("12345Ll$"),
-        role: "adopter"
+        role: "adopter",
+        name: "Agustin",
+        surname: "Campastro",
+        age: 19
     }
 ];
