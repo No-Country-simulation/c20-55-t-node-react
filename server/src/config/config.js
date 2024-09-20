ce75ea6f198da454105af13cfeaa6105a1e152e2
Leonel Angel <leonelgo66@gmail.com>
2024-09-20 12:35:57 -0300

    fix: change port server

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3001;
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://santiagobalbarrey:90VqgZElZIGsS0rd@nocountry.xzhf6.mongodb.net/adoppet"
 export const SECRET_JWT_KEY = process.env.SECRET_JWT_KEY;
 
